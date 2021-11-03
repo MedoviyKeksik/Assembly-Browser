dd5678cea7a45d4040c2b7df7fafe527e5b41fa0
Klim <Severin.Klim@yandex.by>
2021-11-03 18:16:57 +0300

    Added View

---
+++ Assembly Browser/ViewModels/MemberInfoViewModel.cs
@@ -0,0 +1,60 @@
+﻿using AssemblyDataExtractor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_Browser.ViewModels
+{
+    public class MemberInfoViewModel
+    {
+        public object MemberInfoModel { get; set; }
+
+        public string MemberInfo { get; set; }
+
+        public List<MemberInfoViewModel> Children { get; set; } = new();
+
+        public MemberInfoViewModel(NamespaceModel namespaceModel)
+        {
+            MemberInfoModel = namespaceModel;
+            MemberInfo = namespaceModel.ToString();
+
+            foreach (var typeModel in namespaceModel.Types)
+            {
+                Children.Add(new MemberInfoViewModel(typeModel));
+            }
+        }
+
+        private MemberInfoViewModel(TypeModel typeModel)
+        {
+            MemberInfoModel = typeModel;
+            MemberInfo = typeModel.ToString();
+
+            foreach (var memberModel in typeModel.Members)
+            {
+                Children.Add(new MemberInfoViewModel(memberModel));
+            }
+
+            foreach (var memberModel in typeModel.ExtensionMethods)
+            {
+                Children.Add(new MemberInfoViewModel(memberModel));
+            }
+        }
+
+        private MemberInfoViewModel(MemberModel memberModel)
+        {
+            MemberInfoModel = memberModel;
+            MemberInfo = memberModel.ToString();
+
+            if (memberModel is PropertyModel propertyModel)
+            {
+                if (propertyModel.GetMethod != null)
+                    Children.Add(new MemberInfoViewModel(propertyModel.GetMethod));
+
+                if (propertyModel.SetMethod != null)
+                    Children.Add(new MemberInfoViewModel(propertyModel.SetMethod));
+            }
+        }
+    }
+}
---
+++ Assembly Browser/ViewModels/TreeInfoViewModel.cs
@@ -0,0 +1,49 @@
+﻿using AssemblyDataExtractor;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_Browser.ViewModels
+{
+    public class TreeInfoViewModel : INotifyPropertyChanged
+    {
+        private IDataExtractor _dataExtractor;
+
+        public SearchCommand SearchCommand { get; }
+
+        public string AssemblyFilename { get; set; }
+
+        public ObservableCollection<MemberInfoViewModel> MemberInfoViewModels { get; set; } = new();
+
+        public TreeInfoViewModel()
+        {
+            _dataExtractor = new DataExtractor();
+
+            SearchCommand = new SearchCommand(this);
+        }
+
+        public void GetInfoAboutAssembly()
+        {
+            var list = _dataExtractor.GetInformationAboutAssembly(AssemblyFilename);
+
+            foreach (var namespaceModel in list)
+            {
+                MemberInfoViewModels.Add(new MemberInfoViewModel(namespaceModel));
+            }
+
+            OnPropertyChanged(nameof(MemberInfoViewModels));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
